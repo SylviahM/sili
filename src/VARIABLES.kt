26db4a0094670aef3e6d3cf7d4b0bbc42076c673
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-03-05 16:32:12 +0300

    commit

---
+++ src/VARIABLES.kt
@@ -0,0 +1,47 @@
+fun main() {
+    var x: Int=14
+    println(x)
+
+    var fruit = "Banana"
+    println(fruit[0])
+    println(fruit[1])
+    println(fruit[2])
+    println(fruit[3])
+    println(fruit[4])
+    println(fruit[5])
+
+fun main(){
+    var r = "abcd"
+    var y = "Abcd"
+    println(r.capitalize())
+
+}
+    var r = "abcd"
+    var y = "AbcD"
+    println(r.capitalize())
+    println(y.decapitalize())
+
+    var f = "the girl who didn't do her work apropriately is called"
+    var name= "monica"
+    println(f + " " + name)
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
